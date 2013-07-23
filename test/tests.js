42815770ecd714967382e1c227112a33b4375814
Nicholas Calugar <njcalugar@gmail.com>
2013-07-23 15:37:03 -0700

    Apply diff to a different object
    I am using deep-diff to generate a diff object that can be sent to clients when an object changes.

--- test/tests.js
+++ test/tests.js
@@ -92,5 +92,52 @@ describe('deep-diff', function() {
 
 	});
 
-});
+	describe('A target that has nested values', function() {
+		var nestedOne = { noChange: 'same', levelOne: { levelTwo: 'value' } };
+		var nestedTwo = { noChange: 'same', levelOne: { levelTwo: 'another value' } };
 
+		it('shows no differences when compared to itself', function() {
+			expect(deep.diff(nestedOne, nestedOne)).to.be.an('undefined');
+		});
+
+		it('shows the property as removed when compared to an empty object', function() {
+			var diff = deep.diff(nestedOne, empty);
+			expect(diff).to.be.ok();
+			expect(diff.length).to.be(2);
+			expect(diff[0]).to.have.property('kind');
+			expect(diff[0].kind).to.be('D');
+		});
+
+		it('shows the property is changed when compared to an object that has value', function() {
+			var diff = deep.diff(nestedOne, nestedTwo);
+			expect(diff).to.be.ok();
+			expect(diff.length).to.be(1);
+			expect(diff[0]).to.have.property('kind');
+			expect(diff[0].kind).to.be('E');
+		});
+
+		it('shows the property as added when compared to an empty object on left', function() {
+			var diff = deep.diff(empty, nestedOne);
+			expect(diff).to.be.ok();
+			expect(diff.length).to.be(2);
+			expect(diff[0]).to.have.property('kind');
+			expect(diff[0].kind).to.be('N');
+		});
+
+		describe('when diff is applied to a different empty object', function () {
+			var diff = deep.diff(nestedOne, nestedTwo);
+			var result = {};
+
+			it('has result with nested values', function() {
+				deep.applyChange(result, nestedTwo, diff[0]);
+				expect(result.levelOne).to.be.ok();
+				expect(result.levelOne).to.be.an('object');
+				expect(result.levelOne.levelTwo).to.be.ok();
+				expect(result.levelOne.levelTwo).to.eql('another value');
+			});
+
+		});
+
+	});
+
+});
