295b6afffc6c700995a5b52395cf64ffcf774dcd
icesoar <icesoar@hotmail.com>
2016-03-02 16:22:09 +0800

    fix wrong 'undefined' definition under almond.
    'almond' will inject ['require', 'exports', 'module'] dependences, if we define the module with a empty 'deps'. That means the first argument of factory would be 'require' function, rather than the 'undefined' we expected.

--- index.js
+++ index.js
@@ -6,7 +6,9 @@
   'use strict';
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define([], factory);
+    define([], function() {
+      return factory();
+    });
   } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
