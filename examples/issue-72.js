e54b00053324c4773e65c36557d6742f066ad38a
Phillip Clark <phillip@flitbit.com>
2017-04-25 05:54:07 -0600

    added illustration for issue 72 (a question)

---
+++ examples/issue-72.js
@@ -0,0 +1,22 @@
+var diff = require("../");
+
+var before = {
+  data: [1, 2, 3]
+};
+
+var after = {
+  data: [4, 5, 1]
+};
+
+var differences = diff(before, after);
+console.log(differences);
+
+var reverted = differences.reduce(
+  (acc, change) => {
+    diff.revertChange(acc, true, change);
+    return acc;
+  },
+  after
+);
+
+console.log(after);
